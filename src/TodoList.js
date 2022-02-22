8493b4c3886880446e0bbe6c558388ddd265d7fe
smj <tjfaudwo0818@gmail.com>
2022-02-22 07:26:25 +0000

    TodoApp TodoForm TodoList render test ok

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import TodoItem from './TodoItem';
 
 const TodoList = ({todos, onToggle, onRemove}) => {
-  return <ul>{todos.map(todo => (
+  return <ul data-testid="TodoList">{todos.map(todo => (
     <TodoItem todo={todo} key={todo.id} onToggle={onToggle} onRemove={onRemove} />
   ))}
   </ul>;
