9b0f9df9d39cc16bcdc64368101a0608ed990016
smj <tjfaudwo0818@gmail.com>
2022-02-22 03:30:12 +0000

    TodoForm refactoring ok

--- src/TodoForm.test.js
+++ src/TodoForm.test.js
@@ -3,14 +3,25 @@ import { fireEvent, render } from '@testing-library/react';
 import TodoForm from "./TodoForm";
 
 describe('<TodoForm />', () => {
+  const setup = ((props = {}) => {
+    const utils = render(<TodoForm {...props} />);
+    const { getByText, getByPlaceholderText } = utils;
+    const input = getByPlaceholderText('할 일을 입력하세요~');
+    const button = getByText('등록');
+    return {...utils, input, button};
+  })
   it('has input and a button', () => {
-    const {getByText, getByPlaceholderText} = render(<TodoForm />);
-    getByPlaceholderText('할 일을 입력하세요~') // input이 있는지 없는지
-    getByText('등록'); // button이 있는지 없는지
+    const {input, button} = setup();
+    // const {getByText, getByPlaceholderText} = render(<TodoForm />);
+    // getByPlaceholderText('할 일을 입력하세요~') // input이 있는지 없는지
+    // getByText('등록'); // button이 있는지 없는지
+    expect(input).toBeTruthy();
+    expect(button).toBeTruthy();
   })
   it('change input', () => {
-    const {getByPlaceholderText} = render(<TodoForm />);
-    const input = getByPlaceholderText('할 일을 입력하세요~');
+    const {input} = setup();
+    // const {getByPlaceholderText} = render(<TodoForm />);
+    // const input = getByPlaceholderText('할 일을 입력하세요~');
     fireEvent.change(input, {target: {value: 'TDD 배우기'}});
     expect(input).toHaveAttribute('value', 'TDD 배우기');
   })
